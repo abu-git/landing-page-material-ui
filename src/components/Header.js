672240c81fb7a8222f6ca731a8105c8a0eda1a12
Samuel Abu <abu.sammie@gmail.com>
2021-06-16 18:00:36 +0200

    Header and mainpage progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, IconButton, Toolbar } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontFamily: 'Nunito'
+  },
   appbar: {
     background: 'none',
-    fontFamily: 'Nunito'
   },
   appbarTitle: {
     flexGrow: '1'
   },
   colorTitle:{
     color: '#5AFF3D'
+  },
+  container: {
+    textAlign: 'center',
   },
   appbarWrapper: {
     width: '80%',
@@ -20,6 +30,15 @@ const useStyles = makeStyles({
   icon: {
     color: '#fff',
     fontSize: '2rem'
+  },
+  title: {
+    color: 'white',
+    fontSize: '4.5rem'
+
+  },
+  goDown: {
+    color: '#5AFF3D',
+    fontSize: '4rem'
   }
 });
 
@@ -27,7 +46,7 @@ export default function Header () {
     const classes = useStyles();
   
     return(
-      <div>
+      <div className={classes.root}>
         <AppBar className={classes.appbar} elevation={0}>
           <Toolbar className={classes.appbarWrapper}>
             <h1 className={classes.appbarTitle}>
@@ -37,8 +56,16 @@ export default function Header () {
                 <SortIcon className={classes.icon}/>
             </IconButton>
           </Toolbar>
-            
         </AppBar>
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br /> My
+            <span className={classes.colorTitle}>Island.</span>
+          </h1>
+          <IconButton>
+            <ExpandMoreIcon className={classes.goDown} />
+          </IconButton>
+        </div>
       </div>
     )
   }
